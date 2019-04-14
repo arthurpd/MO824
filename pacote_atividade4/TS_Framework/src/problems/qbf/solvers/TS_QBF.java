2355b2baa843a054e5fe68cc41da50d3eadfdbd2
Arthur Pratti Dadalto <arthurpd10@gmail.com>
2019-04-14 11:46:33 -0300

    Task package

---
+++ pacote_atividade4/TS_Framework/src/problems/qbf/solvers/TS_QBF.java
@@ -0,0 +1,197 @@
+package problems.qbf.solvers;
+
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+import metaheuristics.tabusearch.AbstractTS;
+import problems.qbf.QBF_Inverse;
+import solutions.Solution;
+
+
+
+/**
+ * Metaheuristic TS (Tabu Search) for obtaining an optimal solution to a QBF
+ * (Quadractive Binary Function -- {@link #QuadracticBinaryFunction}).
+ * Since by default this TS considers minimization problems, an inverse QBF
+ *  function is adopted.
+ * 
+ * @author ccavellucci, fusberti
+ */
+public class TS_QBF extends AbstractTS<Integer> {
+	
+	private final Integer fake = new Integer(-1);
+
+	/**
+	 * Constructor for the TS_QBF class. An inverse QBF objective function is
+	 * passed as argument for the superclass constructor.
+	 * 
+	 * @param tenure
+	 *            The Tabu tenure parameter.
+	 * @param iterations
+	 *            The number of iterations which the TS will be executed.
+	 * @param filename
+	 *            Name of the file for which the objective function parameters
+	 *            should be read.
+	 * @throws IOException
+	 *             necessary for I/O operations.
+	 */
+	public TS_QBF(Integer tenure, Integer iterations, String filename) throws IOException {
+		super(new QBF_Inverse(filename), tenure, iterations);
+	}
+
+	/* (non-Javadoc)
+	 * @see metaheuristics.tabusearch.AbstractTS#makeCL()
+	 */
+	@Override
+	public ArrayList<Integer> makeCL() {
+
+		ArrayList<Integer> _CL = new ArrayList<Integer>();
+		for (int i = 0; i < ObjFunction.getDomainSize(); i++) {
+			Integer cand = new Integer(i);
+			_CL.add(cand);
+		}
+
+		return _CL;
+
+	}
+
+	/* (non-Javadoc)
+	 * @see metaheuristics.tabusearch.AbstractTS#makeRCL()
+	 */
+	@Override
+	public ArrayList<Integer> makeRCL() {
+
+		ArrayList<Integer> _RCL = new ArrayList<Integer>();
+
+		return _RCL;
+
+	}
+	
+	/* (non-Javadoc)
+	 * @see metaheuristics.tabusearch.AbstractTS#makeTL()
+	 */
+	@Override
+	public ArrayDeque<Integer> makeTL() {
+
+		ArrayDeque<Integer> _TS = new ArrayDeque<Integer>(2*tenure);
+		for (int i=0; i<2*tenure; i++) {
+			_TS.add(fake);
+		}
+
+		return _TS;
+
+	}
+
+	/* (non-Javadoc)
+	 * @see metaheuristics.tabusearch.AbstractTS#updateCL()
+	 */
+	@Override
+	public void updateCL() {
+
+		// do nothing
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * This createEmptySol instantiates an empty solution and it attributes a
+	 * zero cost, since it is known that a QBF solution with all variables set
+	 * to zero has also zero cost.
+	 */
+	@Override
+	public Solution<Integer> createEmptySol() {
+		Solution<Integer> sol = new Solution<Integer>();
+		sol.cost = 0.0;
+		return sol;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * The local search operator developed for the QBF objective function is
+	 * composed by the neighborhood moves Insertion, Removal and 2-Exchange.
+	 */
+	@Override
+	public Solution<Integer> neighborhoodMove() {
+
+		Double minDeltaCost;
+		Integer bestCandIn = null, bestCandOut = null;
+
+		minDeltaCost = Double.POSITIVE_INFINITY;
+		updateCL();
+		// Evaluate insertions
+		for (Integer candIn : CL) {
+			Double deltaCost = ObjFunction.evaluateInsertionCost(candIn, incumbentSol);
+			if (!TL.contains(candIn) || incumbentSol.cost+deltaCost < bestSol.cost) {
+				if (deltaCost < minDeltaCost) {
+					minDeltaCost = deltaCost;
+					bestCandIn = candIn;
+					bestCandOut = null;
+				}
+			}
+		}
+		// Evaluate removals
+		for (Integer candOut : incumbentSol) {
+			Double deltaCost = ObjFunction.evaluateRemovalCost(candOut, incumbentSol);
+			if (!TL.contains(candOut) || incumbentSol.cost+deltaCost < bestSol.cost) {
+				if (deltaCost < minDeltaCost) {
+					minDeltaCost = deltaCost;
+					bestCandIn = null;
+					bestCandOut = candOut;
+				}
+			}
+		}
+		// Evaluate exchanges
+		for (Integer candIn : CL) {
+			for (Integer candOut : incumbentSol) {
+				Double deltaCost = ObjFunction.evaluateExchangeCost(candIn, candOut, incumbentSol);
+				if ((!TL.contains(candIn) && !TL.contains(candOut)) || incumbentSol.cost+deltaCost < bestSol.cost) {
+					if (deltaCost < minDeltaCost) {
+						minDeltaCost = deltaCost;
+						bestCandIn = candIn;
+						bestCandOut = candOut;
+					}
+				}
+			}
+		}
+		// Implement the best non-tabu move
+		TL.poll();
+		if (bestCandOut != null) {
+			incumbentSol.remove(bestCandOut);
+			CL.add(bestCandOut);
+			TL.add(bestCandOut);
+		} else {
+			TL.add(fake);
+		}
+		TL.poll();
+		if (bestCandIn != null) {
+			incumbentSol.add(bestCandIn);
+			CL.remove(bestCandIn);
+			TL.add(bestCandIn);
+		} else {
+			TL.add(fake);
+		}
+		ObjFunction.evaluate(incumbentSol);
+		
+		return null;
+	}
+
+	/**
+	 * A main method used for testing the TS metaheuristic.
+	 * 
+	 */
+	public static void main(String[] args) throws IOException {
+
+		long startTime = System.currentTimeMillis();
+		TS_QBF tabusearch = new TS_QBF(20, 10000, "instances/qbf100");
+		Solution<Integer> bestSol = tabusearch.solve();
+		System.out.println("maxVal = " + bestSol);
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("Time = "+(double)totalTime/(double)1000+" seg");
+
+	}
+
+}
